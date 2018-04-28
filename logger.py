59cc0a5a14e3159e96bb350e09111bbaf1e716b4
r4nx <mod34@yandex.ru>
2018-04-29 01:00:21 +0300

    change license to GPLv3, add readme, add requirements file

--- logger.py
+++ logger.py
@@ -4,11 +4,18 @@
 # telegramchatbot - AI Telegram Chatbot
 # Copyright (C) 2018  Ranx
 
-# This document is the property of Ranx.
-# It is considered confidential and proprietary.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-# This document may not be reproduced or transmitted in any form,
-# in whole or in part, without the express written permission of Ranx.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
 
